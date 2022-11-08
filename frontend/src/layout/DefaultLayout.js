94885a2546e1168bff75e460620ce5cbbb0e013e
yandys09 <yandys03@naver.com>
2022-11-08 20:29:58 +0900

    breadecrumb added

--- frontend/src/layout/DefaultLayout.js
+++ frontend/src/layout/DefaultLayout.js
@@ -5,7 +5,7 @@ import { Header } from "./partials/Header.comp";
 export const DefaultLayout = ({ children }) => {
   return (
     <div className="default-layout">
-      <header className="header">
+      <header className="header mb-2 ">
         <Header />
       </header>
 
